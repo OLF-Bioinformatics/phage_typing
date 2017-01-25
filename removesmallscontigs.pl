89c8fd6eaa9fed098803f67c08ad811a204ea299
duceppemo <marc-olivier.duceppe@inspection.gc.ca>
2017-01-25 16:07:01 -0500

    documentation

--- removesmallscontigs.pl
+++ removesmallscontigs.pl
@@ -1,20 +1,21 @@
 #!/usr/bin/perl
+
 use strict;
 use warnings;
 
-## removesmalls.pl
+# Usage: perl removesmalls.pl 1000 input.fasta > output.fasta
 
 my $minlen = shift or die "Error: `minlen` parameter not provided\n";
 {
-    local $/=">";
+    local $/=">";  # change temporarely the input record separator from "\n" to ">"
     while(<>) {
         chomp;
-        next unless /\w/;
-        s/>$//gs;
+        next unless /\w/;  # skip line if no word character ("\w") is found
+        s/>$//gs;  # 
         my @chunk = split /\n/;
-        my $header = shift @chunk;
-        my $seqlen = length join "", @chunk;
+        my $header = shift @chunk;  # first item in array
+        my $seqlen = length join "", @chunk;  # all the other item of the array
         print ">$_" if($seqlen >= $minlen);
     }
-    local $/="\n";
+    local $/="\n";  #restore input record separator
 }
